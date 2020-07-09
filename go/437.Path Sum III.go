3d322f0f578e4315b9c66be7b2ecef87f9490ae3
imoyakin <imoyaki@outlook.com>
2020-07-09 11:35:07 +0800

    tree

---
+++ go/437.Path Sum III.go
@@ -0,0 +1,30 @@
+// 路径总和 III
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func pathSum(root *TreeNode, sum int) int {
+	storage := make(map[int]int)
+	storage[0] = 1
+	return backtrack(root ,0, sum, storage)
+}
+
+func backtrack(root *TreeNode, cur, sum int, s map[int]int) int {
+	if root == nil {
+		return 0
+	}
+	cur += root.Val
+	var cnt int
+	if v, ok := s[cur-sum]; ok {
+		cnt = v
+	}
+	s[cur] ++
+	cnt += backtrack(root.Left, cur, sum, s)
+	cnt += backtrack(root.Right, cur, sum, s)
+	s[cur] --
+	return cnt
+}
